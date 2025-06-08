2950204db2815044de2a9f5f2c814166dcf97065
Eduardo <edwardviana@gmail.com>
2025-06-08 16:40:36 -0300

    initial

---
+++ Bookify.Domain/Abstractions/Entity.cs
@@ -0,0 +1,11 @@
+﻿namespace Bookify.Domain.Abstractions;
+
+public abstract class Entity(Guid id)
+{
+    private readonly List<IDomainEvent> _domainEvents = [];
+    public Guid Id { get; init; } = id;
+    
+    public IReadOnlyCollection<IDomainEvent> GetDomainEvents() => _domainEvents.ToList();
+    public void ClearDomainEvents() => _domainEvents.Clear();
+    protected void RaiseDomainEvent(IDomainEvent domainEvent) => _domainEvents.Add(domainEvent);
+}
