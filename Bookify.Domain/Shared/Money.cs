35be4968834621816aded69cb74be9281e333922
Eduardo <edwardviana@gmail.com>
2025-06-08 20:28:09 -0300

    criando dominio

--- Bookify.Domain/Shared/Money.cs
+++ Bookify.Domain/Shared/Money.cs
@@ -12,5 +12,6 @@ public record Money(decimal Amount, Currency Currency)
         return new Money(a.Amount + b.Amount, a.Currency);
     }
     
-    public static Money Zero() => new(0, Currency.None);
+    public static Money Zero(Currency currency) => new(0, Currency.None);
+    public bool IsZero() => this == Zero(Currency);
 }
