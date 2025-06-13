f82baf98f98768f3e461bb7f713b6ecbc24fe9c8
Eduardo <edwardviana@gmail.com>
2025-06-13 17:36:04 -0300

    finalizado

--- Bookify.Domain/Abstractions/IDomainEvent.cs
+++
@@ -1,8 +0,0 @@
-﻿using MediatR;
-
-namespace Bookify.Domain.Abstractions;
-
-public interface IDomainEvent: INotification
-{
-    
-}
--- Bookify.Domain/bookings/PricingDetails.cs
+++
@@ -1,9 +0,0 @@
-﻿using Bookify.Domain.Apartments;
-
-namespace Bookify.Domain.Bookings;
-
-public record PricingDetails(
-    Money PriceForPeriod,
-    Money CleaningFee,
-    Money AmenitiesUpCharge,
-    Money TotalPrice);
